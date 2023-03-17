55c581489c7ace72b7b4944b7f8a72dbf9d232e7
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-03-18 00:44:07 +0100

    Search list

--- Common/List.h
+++ Common/List.h
@@ -2,12 +2,17 @@
 
 template<typename T>
 class List {
+protected:
     typedef unsigned char blockSize_t;      // integer type that is capable of storing blockSize
     typedef unsigned long listSize_t;       // max amount of elements in the list
     static const blockSize_t blockSize = 8; // max number of elements allocated in one node
 
     struct Node {
-        T elements[blockSize];
+        struct Element{
+            T value;
+            bool free = true;
+        };
+        Element elements[blockSize];
         blockSize_t elementCount = 0, freeSpace = blockSize;
         Node *previous = nullptr, *next = nullptr;
         void add(const T &element);
@@ -24,17 +29,42 @@ public:
     T popBack();
     T& operator[](listSize_t index);
     long getSize();
-private:
-    Node* getN(listSize_t n);
+protected:
+    typename Node::Element* getN(listSize_t n);
 };
 
 template<typename T>
-typename List<T>::Node *List<T>::getN(List::listSize_t n) {
-    Node* node;
-    if(n > size/2){ // start from the last
-        node = last;
+typename List<T>::Node::Element *List<T>::getN(List::listSize_t n) {
+    if(n > size)
+        return nullptr;
+    Node* node; listSize_t current;
+    if(n < size/2){ // start from the last
+        node = first; current = 0;
+        while(current + node->elementCount <= n){
+            current += node->elementCount;
+            node = node->next;
+        }
+        typename List<T>::Node::Element *element = node->elements;
+        while(current != n && !element->free){
+            element++;
+            if(!element->free && current < n)
+                current++;
+        }
+        return element;
+
     } else {        // start from the first
-        node = first;
+        node = last; current = size-1;
+        while(current - node->elementCount >= n){
+            current -= node->elementCount;
+            node = node->previous;
+        }
+        typename List<T>::Node::Element *element = node->elements;
+        while(current != n && !element->free){
+            element--;
+            if(!element->free && current > n)
+                current--;
+        }
+        return element;
     }
 }
 
@@ -42,11 +72,8 @@ template<typename T>
 long List<T>::getSize() {
     return size;
 }
-
-
 template<typename T>
 List<T>::List() {}
-
 
 template<typename T>
 List<T>::List(T *array, listSize_t size) {
@@ -61,13 +88,17 @@ List<T>::~List() {
 
 template<typename T>
 T &List<T>::operator[](listSize_t index) {
-
+    return getN(index)->value;
 }
 
 template<typename T>
-void List<T>::Node::add(const T &element) {
+void List<T>::Node::add(const T &value) {
     if(freeSpace > 0){
+        Node::Element element;
+        element.value = value;
+        element.free = false;
         elements[blockSize-freeSpace] = element;
+        elementCount++;
         freeSpace--;
     } else{
         throw; // no space in block
--- Common/main.cpp
+++ Common/main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main() {
     List<int> l;
-    List<std::string> l2;
-    List<List<char>> l3;
-
-    l.pushBack(1);
-    l.pushBack(2);
-    cout << l.getSize();
+    //List<std::string> l2;
+    //List<List<char>> l3;
+    for(int i = 0; i < 2137; i++)
+        l.pushBack(i);
+    cout << l.getSize() << endl;
+    cout << l[12] << endl;
+    l[12] = 421421312;
+    cout << l[12] << endl;
 }
