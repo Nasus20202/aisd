a1114ff7e55f2929659b9f6bdc0891bd3ae25ab7
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-03-18 02:10:01 +0100

    simple list

--- Common/List.h
+++ Common/List.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <initializer_list>
 
 template<typename T>
 class List {
@@ -15,7 +16,8 @@ protected:
         Element elements[blockSize];
         blockSize_t elementCount = 0, freeSpace = blockSize;
         Node *previous = nullptr, *next = nullptr;
-        void add(const T &element);
+        void add(T &element);
+        void add(T &&element);
     };
 
     Node *first = nullptr, *last = nullptr;
@@ -23,76 +25,62 @@ protected:
 
 public:
     List();
-    List(T array[], listSize_t size);
+    List(std::initializer_list<T> list);
     ~List();
-    void pushBack(const T &element);
-    T popBack();
+    List(List &other);
+    void pushBack (T &element);
+    void pushBack (T &&element);
     T& operator[](listSize_t index);
-    long getSize();
+    List<T>& operator=(List &other);
+    long getSize() const;
+    void remove(listSize_t index);
 protected:
-    typename Node::Element* getN(listSize_t n);
+    typename Node::Element* getElement(listSize_t n);
+    Node* getNodeOfElement(listSize_t n);
 };
 
-template<typename T>
-typename List<T>::Node::Element *List<T>::getN(List::listSize_t n) {
-    if(n > size)
-        return nullptr;
-    Node* node; listSize_t current;
-    if(n < size/2){ // start from the last
-        node = first; current = 0;
-        while(current + node->elementCount <= n){
-            current += node->elementCount;
-            node = node->next;
-        }
-        typename List<T>::Node::Element *element = node->elements;
-        while(current != n && !element->free){
-            element++;
-            if(!element->free && current < n)
-                current++;
-        }
-        return element;
-
-    } else {        // start from the first
-        node = last; current = size-1;
-        while(current - node->elementCount >= n){
-            current -= node->elementCount;
-            node = node->previous;
-        }
-        typename List<T>::Node::Element *element = node->elements;
-        while(current != n && !element->free){
-            element--;
-            if(!element->free && current > n)
-                current--;
-        }
-        return element;
-    }
-}
-
-template<typename T>
-long List<T>::getSize() {
-    return size;
-}
 template<typename T>
 List<T>::List() {}
 
 template<typename T>
-List<T>::List(T *array, listSize_t size) {
-    for(listSize_t i = 0; i < size; i++)
-        pushBack(array[i]);
+List<T>::List(std::initializer_list<T> list) {
+    for(T elem : list)
+        pushBack(elem);
+}
+
+template<typename T>
+List<T>::List(List &other) : first(nullptr), last(nullptr), size(0){
+    for(int i = 0; i < other.getSize(); i++){
+        pushBack(other[i]);
+    }
 }
 
 template<typename T>
 List<T>::~List() {
-
+    Node *node = first, *copy;
+    while(node != nullptr){
+        copy = node;
+        node = node->next;
+        delete copy;
+    }
 }
 
 template<typename T>
 T &List<T>::operator[](listSize_t index) {
-    return getN(index)->value;
+    return getElement(index)->value;
 }
 
 template<typename T>
-void List<T>::Node::add(const T &value) {
+List<T> &List<T>::operator=(List &other) {
+    List temp = other;
+    std::swap(first, temp.first);
+    std::swap(last, temp.last);
+    size = temp.size;
+    return *this;
+}
+
+template<typename T>
+void List<T>::Node::add(T &value) {
     if(freeSpace > 0){
         Node::Element element;
         element.value = value;
@@ -106,7 +94,21 @@ void List<T>::Node::add(const T &value) {
 }
 
 template<typename T>
-void List<T>::pushBack(const T &element) {
+void List<T>::Node::add(T &&value) {
+    if(freeSpace > 0){
+        Node::Element element;
+        element.value = std::move(value);
+        element.free = false;
+        elements[blockSize-freeSpace] = element;
+        elementCount++;
+        freeSpace--;
+    } else{
+        throw; // no space in block
+    }
+}
+
+template<typename T>
+void List<T>::pushBack(T &element) {
     if(first == nullptr){
         first = new Node();
         last = first;
@@ -124,6 +126,66 @@ void List<T>::pushBack(const T &element) {
 }
 
 template<typename T>
-T List<T>::popBack() {
+void List<T>::pushBack(T &&element) {
+    if(first == nullptr){
+        first = new Node();
+        last = first;
+    }
+    if(last->freeSpace == 0){
+        Node* newNode = new Node();
+        newNode->add(std::move(element));
+        last->next = newNode;
+        newNode->previous = last;
+        last = newNode;
+    } else {
+        last->add(std::move(element));
+    }
+    size++;
+}
 
+
+template<typename T>
+void List<T>::remove(List::listSize_t index) {
+
+}
+
+
+template<typename T>
+typename List<T>::Node::Element *List<T>::getElement(List::listSize_t n) {
+    if(n > size)
+        return nullptr;
+    Node* node; listSize_t current;
+    if(n < size/2){ // start from the last
+        node = first; current = 0;
+        while(current + node->elementCount <= n){
+            current += node->elementCount;
+            node = node->next;
+        }
+        typename List<T>::Node::Element *element = node->elements;
+        while(current != n || element->free){
+            if(!element->free && current < n)
+                current++;
+            element++;
+        }
+        return element;
+
+    } else {        // start from the first
+        node = last; current = size-1;
+        while(current >= node->elementCount && current - node->elementCount >= n){
+            current -= node->elementCount;
+            node = node->previous;
+        }
+        typename List<T>::Node::Element *element = &node->elements[List<T>::blockSize-1]; // start from last one in that block
+        while(current != n || element->free){
+            if(!element->free && current > n)
+                current--;
+            element--;
+        }
+        return element;
+    }
+}
+
+template<typename T>
+long List<T>::getSize() const {
+    return size;
 }
--- Common/main.cpp
+++ Common/main.cpp
@@ -4,12 +4,30 @@ using namespace std;
 
 int main() {
     List<int> l;
-    //List<std::string> l2;
-    //List<List<char>> l3;
-    for(int i = 0; i < 2137; i++)
+    for(int i = 0; i < 10; i++){
         l.pushBack(i);
-    cout << l.getSize() << endl;
-    cout << l[12] << endl;
-    l[12] = 421421312;
-    cout << l[12] << endl;
+    }
+    for(int i = 0; i < l.getSize(); i++){
+        cout << l[i] << " ";
+    }
+    List<int> l2;
+    l2.pushBack(12);
+    l2 = l;
+    cout << endl;
+    for(int i = 0; i < l2.getSize(); i++){
+        cout << l2[i] << " ";
+    }
+    List<List<int>> a;
+    for(int i = 0; i <= 11; i++) {
+        List<int> aa;
+        for (int j = 0; j < i; j++)
+            aa.pushBack(j);
+        a.pushBack(aa);
+    }
+    for(int i = 0; i < a.getSize(); i++){
+        for(int j = 0; j < a[i].getSize(); j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
