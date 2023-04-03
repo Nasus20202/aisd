3a8e0e3a8619c16544700e32bf27d222631a5c02
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-04-03 20:13:22 +0200

    nothing

--- Project2/Map.cpp
+++ Project2/Map.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 Map::Map(int width, int height) : width(width), height(height) {
+
 }
 
 Map::~Map() {
@@ -10,14 +11,6 @@ Map::~Map() {
 }
 
 void Map::readMap() {
-    struct Tile {
-        struct Connection {
-            Tile *tile;
-            int distance;
-        };
-        int x, y; char type; bool visited = false; City *city = nullptr; Vector<Connection> connections;
-        Tile(int x = 0, int y = 0, char type = emptyTile) : x(x), y(y), type(type) {}
-    };
     Vector<Vector<Tile>> tileMap(height); int cityCount = 0;
     for(int i = 0; i < height; i++){
         tileMap.pushBack(Vector<Tile>(width));
@@ -30,54 +23,61 @@ void Map::readMap() {
             tileMap[i].pushBack(Tile(j, i, c));
         }
     }
-    cities.resize(cityCount);
     // read cities
-    {
-        Vector<Vector<Tile>> citiesTempMap = tileMap;
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                Tile *tile = &citiesTempMap[i][j];
-                if (tile->type != cityTile)
-                    continue;
-                // create a new city
-                if (tile->type == cityTile) {
-                    cities.pushBack(City());
-                    City *currentCity = &cities[cities.size()-1];
-                    // read city name
-                    int startingX, startingY;
-                    for(int y = -1; y <= 1; y++){
-                        for(int x = -1; x <= 1; x++){
-                            if(i+y < 0 || i+y >= height || j+x < 0 || j+x >= width)
-                                continue;
-                            char value = citiesTempMap[i+y][j+x].type;
-                            if(value == cityTile || value == roadTile || value == emptyTile)
-                                continue;
-                            startingX = j+x;
-                            startingY = i+y;
-                            break;
-                        }
-                    }
-                    // move starting X as much to the left as possible
-                    while (startingX > 0){
-                        char value = citiesTempMap[startingY][startingX-1].type;
-                        if(value == cityTile || value == roadTile || value == emptyTile)
-                            break;
-                        startingX--;
-                    }
-                    while(startingX < width){
-                        char value = citiesTempMap[startingY][startingX].type;
-                        if(value == cityTile || value == roadTile || value == emptyTile)
-                            break;
-                        currentCity->name.add(value);
-                        startingX++;
+    cities.resize(cityCount);
+    loadCities(tileMap);
+    Queue<Tile*> queue;
+    //queue.enqueue(&tileMap[rootY][rootY]);
+    //std::cout << queue.peek()->x << queue.peek()->y << std::endl;
+    for(int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++)
+            std::cout << tileMap[i][j].type;
+        std::cout << std::endl;
+    }
+    for(int i = 0; i < cities.length(); i++){
+        std::cout << cities[i].name << std::endl;
+    }
+}
+
+void Map::loadCities(Vector<Vector<Tile>> &tileMap) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            Tile *tile = &tileMap[i][j];
+            if (tile->type != cityTile)
+                continue;
+            // create a new city
+            if (tile->type == cityTile) {
+                cities.pushBack(City());
+                City *currentCity = &cities[cities.size() - 1];
+                // read city name
+                int startingX, startingY;
+                for (int y = -1; y <= 1; y++) {
+                    for (int x = -1; x <= 1; x++) {
+                        if (i + y < 0 || i + y >= height || j + x < 0 || j + x >= width)
+                            continue;
+                        char value = tileMap[i + y][j + x].type;
+                        if (value == cityTile || value == roadTile || value == emptyTile)
+                            continue;
+                        startingX = j + x;
+                        startingY = i + y;
+                        break;
                     }
                 }
+                // move starting X as much to the left as possible
+                while (startingX > 0) {
+                    char value = tileMap[startingY][startingX - 1].type;
+                    if (value == cityTile || value == roadTile || value == emptyTile)
+                        break;
+                    startingX--;
+                }
+                while (startingX < width) {
+                    char value = tileMap[startingY][startingX].type;
+                    if (value == cityTile || value == roadTile || value == emptyTile)
+                        break;
+                    currentCity->name.add(value);
+                    startingX++;
+                }
             }
-        }
-        for(int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++)
-                std::cout << (char) citiesTempMap[i][j].type;
-            std::cout << std::endl;
         }
     }
 }
--- Project2/Map.h
+++ Project2/Map.h
@@ -11,14 +11,19 @@ struct City {
     String name;
 };
 
-
 class Map {
 private:
-    static const char emptyTile = '.', cityTile = '*', roadTile = '#';
+    struct Tile {
+        char type; int x, y; bool visited = false;
+        Tile(int x = 0, int y = 0,char type = Map::emptyTile) : type(type), x(x), y(y) {}
+    };
     int width, height;
     Vector<City> cities;
 public:
+    static const char emptyTile = '.', cityTile = '*', roadTile = '#';
     Map(int width, int height);
     ~Map();
     void readMap();
+private: // helpers
+    void loadCities(Vector<Vector<Tile>>&);
 };
